eaeda57e6d724b46208b6d2725a3a5e4a30a3183
fahad.jadiya <fahad.jadiya@techaxion.com>
2025-09-09 10:49:51 +0530

    PDF generated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductPdfController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products/brochure', [ProductPdfController::class, 'generate'])
+    ->name('products.brochure');
