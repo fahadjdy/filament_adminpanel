ae2eab9e8fc8e2ec2426b8f0b91877cb6f59949c
fahadjdy <fahadjdy12@gmail.com>
2025-09-17 23:03:43 +0530

    admin panel issue solved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\CompanyProfile;
+use Illuminate\Support\Facades\View;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+          $profile = CompanyProfile::first();
+        View::share('profile', $profile);
     }
 }
